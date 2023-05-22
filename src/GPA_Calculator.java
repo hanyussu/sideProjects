5011c8b99f2ffff6e69387bfbd50f9ea2ac533c1
hanyussu <97819533+hanyussu@users.noreply.github.com>
2023-05-22 10:34:48 +0800

    second commit

--- src/GPA_Calculator.java
+++ src/GPA_Calculator.java
@@ -1,9 +1,24 @@
+import java.util.*;
 
 public class GPA_Calculator {
+	
+	HashMap<String, Double> data = new HashMap<>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hellow world");
+	}
+	
+	public void getCredits() {
+		
+	}	
+	
+	public void getAmountOfClasses() {
+		
+	}
+	
+	public void algorithm () {
+		
 	}
 
 }
